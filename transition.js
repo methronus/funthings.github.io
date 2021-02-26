a175b8d29e970661a66c6f7b93f0c356b7415deb
methronus <44928592+methronus@users.noreply.github.com>
2021-02-25 20:28:27 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -1,13 +1,28 @@
-    function Divs() {
-        var divs= $('#parent div'),
-            now = divs.filter(':visible'),
-            next = now.next().length ? now.next() : divs.first(),
-            speed = 1000;
-    
-        now.fadeOut(speed);
-        next.fadeIn(speed);
-    }
-    
-    $(function () {
-        setInterval(Divs, 1400);
-    });
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
